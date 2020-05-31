10186e42087b49dea0c5f09f087ed306f9e803f0
Likeside <Canuckvisa@gmail.com>
2020-05-31 19:19:14 +0300

    Third commit

---
+++ UtiCompanion/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UtiCompanion.Model;
+
+namespace UtiCompanion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Uti uti = new Uti("Тестовая утилита");
+
+        public void UpdateInfo()
+        {
+            lblSpecName.Content = uti.ReturnCurrentSpec().SpecName;
+            lblSpecDescr.Content = uti.ReturnCurrentSpec().SpecDescr;
+        }
+        private void addSpec_Click(object sender, RoutedEventArgs e)
+        {
+            
+            uti.AddSpec();
+            uti.AddSpec("вторая спек", "описание второй спек");
+            
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            uti.PlusCurrentSpec();
+            UpdateInfo();
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            uti.MinusCurrentSpec();
+            UpdateInfo();
+        }
+
+        private void btnChangeSpecName_Click(object sender, RoutedEventArgs e)
+        {
+            uti.ReturnCurrentSpec().RenameSpec(tbSpecName.Text);
+        }
+    }
+}
---
+++ UtiCompanion/Model/Uti.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtiCompanion.Model
+{
+    class Uti
+    {
+       string UtiName { get; set; }
+       List<Spec> spec = new List<Spec>(); // Список особенностей утилиты
+       public int currentSpec = 0; // Переменная, отвечающая за текущую утилиту
+
+        public Uti (string utiName)
+        {
+            this.UtiName = utiName;
+        }
+
+
+        //Нестед класс, содержащий в себе особенность утилиты, состоящий из названия особенности и её описания
+       public class Spec {
+            public string SpecName { get; set; }
+            public string SpecDescr { get; set; }
+
+            public Spec()
+            {
+                this.SpecName = "Введите название";
+                this.SpecDescr = "Введите описание";
+            }
+            public Spec(string specname)
+            {
+                this.SpecName = specname;
+                this.SpecDescr = "Введите описание";
+            }
+            public Spec (string specname, string specdescr)
+            {
+                this.SpecName = specname;
+                this.SpecDescr = specdescr;
+            }
+
+            // Переименовать особенность
+            public void RenameSpec(string specName)
+            {
+                this.SpecName = specName;
+            }
+
+            // Изменить описание
+            public void ChangeDescr(string descr)
+            {
+                this.SpecDescr = descr;
+            }
+        }
+
+        // Добавить особенность
+       public void AddSpec ()
+        {
+            Spec spec = new Spec();
+            this.spec.Add(spec);
+        }
+
+        public void AddSpec(string specname, string specdescr)
+        {
+            Spec spec = new Spec(specname, specdescr);
+            this.spec.Add(spec);
+        }
+
+        // Удалить особенность
+       public void RemoveSpec(Spec spec)
+        {
+            this.spec.Remove(spec);
+        }
+
+        // Получить текущую особенность
+        public Spec ReturnCurrentSpec ()
+        {
+            Spec spec = this.spec[currentSpec];
+            return spec;
+        }
+
+        // Перейти к следующей особенности
+        public void PlusCurrentSpec()
+        {
+            if (currentSpec < spec.Count-1)
+            {
+                currentSpec++;
+            }
+            else 
+            {
+                currentSpec = spec.Count-1;
+            }
+        }
+
+        // Перейти к предыдущей особенности
+        public void MinusCurrentSpec()
+        {
+            if (currentSpec > 0)
+            {
+                currentSpec--;
+            }
+            else if (currentSpec <= 0)
+            {
+                currentSpec = 0;
+            }
+        }
+    }
+}
